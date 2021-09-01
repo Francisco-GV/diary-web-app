9f7307e4e7723c9a5c29f32b687a79e6922ecfeb
Francisco-GV <francisco_gasca@hotmail.com>
2021-09-01 18:47:45 -0500

    First commit. Added maven stuff and DiaryWebAppApplication class.

---
+++ src/main/java/com/frank/diarywebapp/DiaryWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.frank.diarywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frank/diarywebapp/DiaryWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.diarywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
